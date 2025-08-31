dd474e49beb5599c744dbab709ca9ff2538395a4
Hritish Mahajan <hritish.mahajan@gmail.com>
2025-09-01 02:07:56 +0400

    Few More Fixes for Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-import { withCloudflarePages } from "@cloudflare/next-on-pages";
+import pkg from "@cloudflare/next-on-pages";
+const { withCloudflarePages } = pkg;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	experimental: { appDir: true },
-	// …any other Next.js options you need
 };
+
 export default withCloudflarePages(nextConfig);
